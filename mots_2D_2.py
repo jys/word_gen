8445416fbe9e7498ea37611ea7a2c86dbd92d3ef
scls19fr <s.celles@gmail.com>
2015-11-04 18:29:09 +0100

    Fixed

--- mots_2D_2.py
+++ mots_2D_2.py
@@ -10,7 +10,7 @@ import os
 
 def process(lang, codec):
     filepath = os.path.join("words", "%s.txt" % lang)
-    outfile = os.path.join("ouputs", "%s.txt" % lang)
+    outfile = os.path.join("outputs", "%s.txt" % lang)
     probafile = os.path.join("counts", "%s.bin" % lang)
 
     dico = []
@@ -20,37 +20,36 @@ def process(lang, codec):
         
     count = np.fromfile(probafile, dtype="int32").reshape(256,256,256)
 
-    s=count.sum(axis=2)
-    st=np.tile(s.T, (256,1,1)).T
-    p=count.astype('float') / st
-    p[np.isnan(p)]=0
+    s = count.sum(axis=2)
+    st = np.tile(s.T, (256,1,1)).T
+    p = count.astype('float') / st
+    p[np.isnan(p)] = 0
 
     #%%
-    f = codecs.open(outfile, "w", codec)
+    with codecs.open(outfile, "w", codec) as f:
+        K = 100
+        for TGT in range(12,13):
 
-    K = 100
-    for TGT in range(12,13):
-
-    #K = 100
-    #for TGT in range(4,11):
-        total = 0
-        while total<100:
-            i=0
-            j=0
-            res = u''
-            while not j==10:
-                k=choice(range(256),1,p=p[i,j,:])[0]
-                res = res + chr(k)
-                i=j
-                j=k
-            if len(res) == 1 + TGT:
-                x=res[:-1]
-                if res[:-1] in dico:
-                    x=res[:-1] + "*"
-                total += 1
-                print(x)
-                f.write(x + "\n")
-    f.close()
+        #K = 100
+        #for TGT in range(4,11):
+            total = 0
+            while total < 100:
+                i = 0
+                j = 0
+                res = u''
+                while not j==10:
+                    k=choice(range(256),1,p=p[i,j,:])[0]
+                    #res = res + chr(k)
+                    res = res + unichr(k)
+                    i = j
+                    j = k
+                if len(res) == 1 + TGT:
+                    x = res[:-1]
+                    if res[:-1] in dico:
+                        x = res[:-1] + "*"
+                    total += 1
+                    print(x)
+                    f.write(x + "\n")
 
 @click.command()
 @click.option('--lang', default="FR", help='Language')
